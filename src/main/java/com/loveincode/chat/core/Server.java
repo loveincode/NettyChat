d36c9f870bd7aedf00f6d001192b5a2454fda1c3
loveincode <huonefl@163.com>
2018-04-01 15:05:49 +0800

    init | use study Netty

---
+++ src/main/java/com/loveincode/chat/core/Server.java
@@ -0,0 +1,8 @@
+package com.loveincode.chat.core;
+
+public interface Server {
+
+    void start();
+
+    void shutdown();
+}
---
+++ src/main/java/com/loveincode/chat/util/Constants.java
@@ -0,0 +1,8 @@
+package com.loveincode.chat.util;
+
+public class Constants {
+
+    public static String DEFAULT_HOST = "localhost";
+    public static int DEFAULT_PORT = 9090;
+    public static String WEBSOCKET_URL = "ws://localhost:9090/websocket";
+}
